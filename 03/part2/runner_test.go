64001543ec97a4793bb66ac277d2995c1239d728
Abel Tay <abel.tay@gmail.com>
2021-12-03 16:41:50 +0800

    Refactor to use tests to run the program instead
    This will make it easy to test my algorithm against the examples, which
    will hopefully reduce submission error rate.
    
    Credits to: https://github.com/krmaxwell/aoc2021

---
+++ 03/part2/runner_test.go
@@ -0,0 +1,55 @@
+package part2
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"testing"
+)
+
+func TestRunner(t *testing.T) {
+	t.Run("example input", func(t *testing.T) {
+		data := []string{
+			"00100",
+			"11110",
+			"10110",
+			"10111",
+			"10101",
+			"01111",
+			"00111",
+			"11100",
+			"10000",
+			"11001",
+			"00010",
+			"01010",
+		}
+		want := 230
+		got := Runner(data)
+
+		if got != want {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+
+	t.Run("full input", func(t *testing.T) {
+		data := readFullData(t, "../input.txt")
+		got := Runner(data)
+		fmt.Println(got, "is the answer")
+	})
+}
+
+func readFullData(t *testing.T, fname string) []string {
+	t.Helper()
+	f, err := os.Open(fname)
+	if err != nil {
+		t.Fatalf("Could not open %q: %q", fname, err)
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	var data []string
+	for scanner.Scan() {
+		data = append(data, scanner.Text())
+	}
+	return data
+}
