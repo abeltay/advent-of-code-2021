4cedf591d91420dd470b0bcd2f9000aa07adbdd2
Abel Tay <abel.tay@gmail.com>
2021-12-05 15:50:10 +0800

    Add day 5 part 2 solution

---
+++ 05/part2/runner.go
@@ -0,0 +1,122 @@
+package part2
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+)
+
+type coord struct {
+	x, y int
+}
+
+// Runner runs the algorithm to get the answer
+func Runner(data []string) int {
+	var (
+		start, end []coord
+		maxX, maxY int
+	)
+	for i := range data {
+		sarr := strings.Split(data[i], " -> ")
+
+		startCoord := strings.Split(sarr[0], ",")
+		x, err := strconv.Atoi(startCoord[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+		y, err := strconv.Atoi(startCoord[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+		start = append(start, coord{x, y})
+		if x > maxX {
+			maxX = x
+		}
+		if y > maxY {
+			maxY = y
+		}
+
+		endCoord := strings.Split(sarr[1], ",")
+		x, err = strconv.Atoi(endCoord[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+		y, err = strconv.Atoi(endCoord[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+		end = append(end, coord{x, y})
+		if x > maxX {
+			maxX = x
+		}
+		if y > maxY {
+			maxY = y
+		}
+	}
+	// fmt.Println(start)
+	// fmt.Println(end)
+
+	maxX++
+	maxY++
+	ocean := make([][]int, maxY)
+	for i := range ocean {
+		ocean[i] = make([]int, maxX)
+	}
+
+	for i := range start {
+		if start[i].x == end[i].x {
+			j := start[i].y
+			k := end[i].y
+			if start[i].y > end[i].y {
+				j = end[i].y
+				k = start[i].y
+			}
+			for j <= k {
+				ocean[j][start[i].x]++
+				j++
+			}
+			continue
+		}
+		if start[i].y == end[i].y {
+			j := start[i].x
+			k := end[i].x
+			if start[i].x > end[i].x {
+				j = end[i].x
+				k = start[i].x
+			}
+			for j <= k {
+				ocean[start[i].y][j]++
+				j++
+			}
+			continue
+		}
+		drawDiagonal(ocean, start[i], end[i])
+	}
+	// output.TwoDimensionalIntArray(ocean)
+	var count int
+	for i := range ocean {
+		for j := range ocean[i] {
+			if ocean[i][j] > 1 {
+				count++
+			}
+		}
+	}
+	return count
+}
+
+func drawDiagonal(ocean [][]int, start, end coord) {
+	if start.y > end.y {
+		drawDiagonal(ocean, end, start)
+		return
+	}
+	for i, j := start.y, start.x; i <= end.y; i++ {
+		fmt.Printf("coords: %v %v %d %d\n", start, end, i, j)
+		ocean[i][j]++
+		if start.x < end.x {
+			j++
+		} else {
+			j--
+		}
+	}
+}
---
+++ 05/part2/runner_test.go
@@ -0,0 +1,53 @@
+package part2
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"testing"
+)
+
+func TestRunner(t *testing.T) {
+	t.Run("example input", func(t *testing.T) {
+		data := []string{
+			"0,9 -> 5,9",
+			"8,0 -> 0,8",
+			"9,4 -> 3,4",
+			"2,2 -> 2,1",
+			"7,0 -> 7,4",
+			"6,4 -> 2,0",
+			"0,9 -> 2,9",
+			"3,4 -> 1,4",
+			"0,0 -> 8,8",
+			"5,5 -> 8,2",
+		}
+		want := 12
+		got := Runner(data)
+
+		if got != want {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+
+	t.Run("full input", func(t *testing.T) {
+		data := readFullData(t, "../input.txt")
+		got := Runner(data)
+		fmt.Println(got, "is the answer")
+	})
+}
+
+func readFullData(t *testing.T, fname string) []string {
+	t.Helper()
+	f, err := os.Open(fname)
+	if err != nil {
+		t.Fatalf("Could not open %q: %q", fname, err)
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	var data []string
+	for scanner.Scan() {
+		data = append(data, scanner.Text())
+	}
+	return data
+}
